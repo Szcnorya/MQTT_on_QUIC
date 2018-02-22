cae9f0d398d5cb0f25ca930ed50ecd344b427f34
Szcnorya <sikkyzhao@gmail.com>
2018-02-21 21:28:07 -0800

    Measurement script update

--- Performance.py
+++ Performance.py
@@ -12,13 +12,12 @@ from mininet.link import TCLink
 from mininet.clean import Cleanup
 #setLogLevel("info")
 
-POOL_SIZE = 8
-RUN_TIME = 5
-NUM_RUNS = 1
+RUN_TIME = 30
 
-DELAYS = (10,20,50,100,200,500)
-BANDWIDTHS = (5,10,25,50,100)
-LOSSES = (0,)
+DELAYS = (10,)
+BANDWIDTHS = (50,75,100)
+LOSSES = (0,1,2)
+PAYLOADS = (50,256,1024,2048)
 
 class SimpleTopology(Topo):
 
@@ -35,8 +34,8 @@ class SimpleTopology(Topo):
     def create_net(**opts):
         return Mininet(topo=SimpleTopology(**opts), link=TCLink)
 
-def performance_test(srv_cmd, clt_cmd, brok_cmd, run_index, repeat_times, **opts):
-    print("Starting performance test run %s with parameters %s" % (run_index, opts))
+def performance_test(srv_cmd, clt_cmd, brok_cmd, run_index, repeat_times, payload, **opts):
+    print("Starting performance test run %s with parameters %s and payload %d" % (run_index, opts,payload))
     net = SimpleTopology.create_net(**opts)
     net.start()
     h1 = net.get('h1')
@@ -44,27 +43,32 @@ def performance_test(srv_cmd, clt_cmd, brok_cmd, run_index, repeat_times, **opts
     h3 = net.get('h3')
     h3.sendCmd(brok_cmd)
     time.sleep(1)
-    ofile = open('meas/server_run_%s' % ('_'.join("%s" % val for (key,val) in opts.iteritems())), 'w')
+    ofile = open('meas/server_run_%s_%s' % ('_'.join("%s" % val for (key,val) in opts.iteritems()),payload), 'w')
     print("Running server:\n%s" % srv_cmd)
-    h1.sendCmd(srv_cmd + " 2>error_log")
+    h1.sendCmd(srv_cmd)
+    payload = "A"*payload
     time.sleep(1)
     log = ""
     avg = 0.0
     for i in range(repeat_times):
         start = time.time()
-        h2.sendCmd(clt_cmd)
-        _ = h1.monitor(timeoutms=RUN_TIME * 2 * 1000)
-        _ = h2.monitor(timeoutms=RUN_TIME * 2 * 1000)
-        det = time.time()-start
-        det *= 1000
-        log = log + "#{0}:{1}\n".format(i,det)
-        avg += det
-	if det/1000 > (RUN_TIME * 2):
-            print("Started at %s and it's now %s, that's %s seconds later" % (start, time.time(), time.time() - start))
-            errfile = open('error_log', 'a')
-            errfile.write("In run %s test with parameters %s did timeout\n" % (run_index, opts))
-            errfile.close()
-            break
+        h2.sendCmd(clt_cmd+payload)
+        print("###"+str(i)+"###:\n")
+        while(h1.waiting):
+            ret = h1.monitor()
+            det = time.time()-start
+            det *= 1000
+            print(ret)
+            print(str(det)+"\n")
+            if(ret=='\r\n'):
+                log = log + "#{0}:{1}\n".format(i,det)
+                avg += det
+                break 
+            elif det > (RUN_TIME * 2 * 1000):
+                print("Started at %s and it's now %s, that's %s ms later" % (start, time.time(), det))
+                break
+        while(h2.waiting):
+            _ = h2.monitor(timeoutms=RUN_TIME * 2 * 1000)
     for host in [h1,h2,h3]:
         host.sendInt()
         while(host.waiting):
@@ -100,9 +104,10 @@ def run_all_tests_sequentially():
     for delay in DELAYS:
         for bw in BANDWIDTHS:
             for loss in LOSSES:
-                performance_test('./mosquitto-1.4.14/client/mosquitto_sub -h 192.168.0.3 -t test -C 10', "./mosquitto-1.4.14/client/mosquitto_pub -h 192.168.0.3 -t test -m Aloha","mosquitto", run_index, 10, delay=str(delay)+"ms", bw=bw, loss=loss)
-                run_index+=1
-                Cleanup.cleanup()
+                for pl in PAYLOADS:
+                    performance_test('./mosquitto-1.4.14/client/mosquitto_sub -h 192.168.0.3 -q 1 -t test -C 30', "./mosquitto-1.4.14/client/mosquitto_pub -h 192.168.0.3 -q 1 -t test -m ","mosquitto", run_index, 30, pl, delay=str(delay)+"ms", bw=bw, loss=loss)
+                    run_index+=1
+                    Cleanup.cleanup()
                 
 def run_cli():
     net = SimpleTopology.create_net()
